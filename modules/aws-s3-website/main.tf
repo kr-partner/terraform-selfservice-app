c2f6b45c85d1ece5b59a34f4f0bc8a6deecd1013
Hyungwook Yu <105044562+hyungwook0221@users.noreply.github.com>
2023-12-26 00:27:11 +0900

    source ${path.module} added

--- modules/aws-s3-website/main.tf
+++ modules/aws-s3-website/main.tf
@@ -82,7 +82,7 @@ resource "aws_s3_object" "index" {
 resource "aws_s3_object" "error" {
   bucket       = aws_s3_bucket.s3_bucket.id
   key          = "error.html"
-  source       = "${path.module}www/error.html"
+  source       = "${path.module}/www/error.html"
   content_type = "text/html"
 
   etag = filemd5("${path.module}/www/error.html")
